3f379cda984ce4c31857a2ee97f293b62069a405
Facu4chll <facundotomas69@gmail.com>
2022-02-18 18:42:53 -0300

    new commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -89,7 +89,7 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  return num > 50;
 }
 
 function obtenerResto(x, y) {
