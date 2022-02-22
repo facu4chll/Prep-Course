827c5905ea534f646e6c350d1de6b00f0c63f97f
Facu4chll <facundotomas69@gmail.com>
2022-02-22 19:02:16 -0300

    that guy can enter the event

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -15,6 +15,12 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if(19 >= 18) {
+    return "Allowed";
+  }
+  else {
+    return "Not allowed";
+  }
 }
   
 function conection(status) {
