1de6df13371758615b07b50b2a57a6b99d486de6
Facu4chll <facundotomas69@gmail.com>
2022-03-07 18:04:13 -0300

    commit because yeah

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -27,7 +27,7 @@ const nuevoModulo = 21 % 5 === 1;
 
 function devolverString(str) {
   // "Return" la string provista: str
-  // Tu código: https://github.com/facu4chll/hello-world.git
+  // Tu código: 
  return str;  
 }
 
