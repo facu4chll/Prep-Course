a2c2964d0c3c4a98c911d7054da95a3b8fe219c9
Facu4chll <facundotomas69@gmail.com>
2022-04-09 16:57:51 -0300

    boolean just in case

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -96,6 +96,10 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  if (usuario.password === password) {
+    return true;
+  }
+  return false;
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
